4ba06e9ddd0fc4ce67310086047f3d3f12b5f52c
Vlade <nekaytsu@gmail.com>
2025-04-11 15:46:12 -0300

    Adição no Perosnagem.java da lógica de força e defesa

--- src/main/java/com/rpg/rpgmanager/model/Personagem.java
+++ src/main/java/com/rpg/rpgmanager/model/Personagem.java
@@ -3,9 +3,16 @@ package com.rpg.rpgmanager.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList; import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity @Data @NoArgsConstructor @AllArgsConstructor public class Personagem { @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Personagem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
     private String nomeAventureiro;
@@ -20,4 +27,11 @@ import java.util.ArrayList; import java.util.List;
     @OneToMany(mappedBy = "personagem", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemMagico> itensMagicos = new ArrayList<>();
 
+    public int getForcaTotal() {
+        return forcaBase + itensMagicos.stream().mapToInt(ItemMagico::getForca).sum();
+    }
+
+    public int getDefesaTotal() {
+        return defesaBase + itensMagicos.stream().mapToInt(ItemMagico::getDefesa).sum();
+    }
 }
